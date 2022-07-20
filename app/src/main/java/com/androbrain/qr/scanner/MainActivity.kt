c9413928aae8261beaeefb6b4ccf2311de681175
Michal Ankiersztajn <aandrobrain@gmail.com>
2022-07-20 22:56:41 +0200

    Initial commit

---
+++ app/src/main/java/com/androbrain/qr/scanner/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.androbrain.qr.scanner
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.androbrain.qr.scanner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        TODO setup bottomNavigation bar with camera special selection in the middle
+    }
+}
