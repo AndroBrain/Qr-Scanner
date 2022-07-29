68637ca152012e43060108681d109af2fbe43a11
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-07-29 18:20:11 +0200

    Setup GeoPointFragment.kt

--- app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/geo_point/GeoPointFragment.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/geo_point/GeoPointFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.androbrain.qr.scanner.R
+import com.androbrain.qr.scanner.data.geo_point.GeoPointModel
 import com.androbrain.qr.scanner.databinding.FragmentGeoPointBinding
+import com.androbrain.qr.scanner.feature.barcodes.controller.BarcodeController
+import com.androbrain.qr.scanner.feature.barcodes.util.BarcodesUtil
+import com.androbrain.qr.scanner.util.share.shareRaw
 
 class GeoPointFragment : Fragment() {
     private var _binding: FragmentGeoPointBinding? = null
     private val binding get() = _binding!!
     private val args: GeoPointFragmentArgs by navArgs()
+    private val controller by lazy { BarcodeController() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +27,55 @@ class GeoPointFragment : Fragment() {
     ): View {
         _binding = FragmentGeoPointBinding.inflate(inflater)
         setupViews()
+        setupActions()
         return binding.root
     }
 
     private fun setupViews() = with(binding) {
         val geoPointModel = args.geoPointModel
+        textTitle.text = geoPointModel.display ?: getString(R.string.screen_geo_point)
+        recycler.setController(controller)
+        controller.info = createControllerInput(geoPointModel)
+    }
+
+    private fun createControllerInput(geoPointModel: GeoPointModel) = listOfNotNull(
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_scan_date,
+            content = geoPointModel.scanDate.toString(),
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.geo_point_latitude,
+            content = geoPointModel.latitude.toString(),
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.geo_point_longitude,
+            content = geoPointModel.longitude.toString(),
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_display,
+            content = geoPointModel.display,
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_raw,
+            content = geoPointModel.raw,
+        ),
+    )
+
+    private fun setupActions() = with(binding) {
+        toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+        toolbar.menu.findItem(R.id.item_share).apply {
+            val geoPointModel = args.geoPointModel
+            isVisible = !geoPointModel.raw.isNullOrBlank()
+            if (geoPointModel.raw != null && geoPointModel.raw.isNotBlank()) {
+                setOnMenuItemClickListener {
+                    requireContext().shareRaw(
+                        subject = geoPointModel.display ?: geoPointModel.display,
+                        raw = geoPointModel.raw
+                    )
+                    true
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
