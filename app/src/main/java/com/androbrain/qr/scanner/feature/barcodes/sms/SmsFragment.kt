fe82280c2e5c4c180e609d40246aa3aac1448106
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-07-29 17:59:32 +0200

    Setup SmsFragment.kt

--- app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/sms/SmsFragment.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/sms/SmsFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.androbrain.qr.scanner.R
+import com.androbrain.qr.scanner.data.sms.SmsModel
 import com.androbrain.qr.scanner.databinding.FragmentSmsBinding
+import com.androbrain.qr.scanner.feature.barcodes.controller.BarcodeController
+import com.androbrain.qr.scanner.feature.barcodes.util.BarcodesUtil
+import com.androbrain.qr.scanner.util.share.shareRaw
 
 class SmsFragment : Fragment() {
     private var _binding: FragmentSmsBinding? = null
     private val binding get() = _binding!!
     private val args: SmsFragmentArgs by navArgs()
+    private val controller by lazy { BarcodeController() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +27,55 @@ class SmsFragment : Fragment() {
     ): View {
         _binding = FragmentSmsBinding.inflate(inflater)
         setupViews()
+        setupActions()
         return binding.root
     }
 
     private fun setupViews() = with(binding) {
         val smsModel = args.smsModel
+        textTitle.text = smsModel.display ?: getString(R.string.screen_sms)
+        recycler.setController(controller)
+        controller.info = createControllerInput(smsModel)
+    }
+
+    private fun createControllerInput(smsModel: SmsModel) = listOfNotNull(
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_scan_date,
+            content = smsModel.scanDate.toString(),
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.sms_message,
+            content = smsModel.message,
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.sms_phone,
+            content = smsModel.phoneNumber,
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_display,
+            content = smsModel.display,
+        ),
+        BarcodesUtil.getBarcodeCardInputOrNull(
+            title = R.string.barcodes_raw,
+            content = smsModel.raw,
+        ),
+    )
+
+    private fun setupActions() = with(binding) {
+        toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+        toolbar.menu.findItem(R.id.item_share).apply {
+            val smsModel = args.smsModel
+            isVisible = !smsModel.raw.isNullOrBlank()
+            if (smsModel.raw != null && smsModel.raw.isNotBlank()) {
+                setOnMenuItemClickListener {
+                    requireContext().shareRaw(
+                        subject = args.smsModel.display ?: args.smsModel.phoneNumber,
+                        raw = smsModel.raw
+                    )
+                    true
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
