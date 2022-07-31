d90d64f64e4bf7592529b7ddf0ac181c400a158d
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-07-31 18:20:43 +0200

    Setup toolbar actions and title in ContactInfoFragment.kt

--- app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/contact_info/ContactInfoFragment.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/barcodes/contact_info/ContactInfoFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.androbrain.qr.scanner.R
 import com.androbrain.qr.scanner.databinding.FragmentContactInfoBinding
+import com.androbrain.qr.scanner.feature.barcodes.util.BarcodesUtil.setupShare
 
 class ContactInfoFragment : Fragment() {
     private var _binding: FragmentContactInfoBinding? = null
@@ -21,10 +24,25 @@ class ContactInfoFragment : Fragment() {
     ): View {
         _binding = FragmentContactInfoBinding.inflate(inflater)
         setupViews()
+        setupActions()
         return binding.root
     }
 
     private fun setupViews() = with(binding) {
+        textTitle.text = if (contactInfoModel.title.isNullOrBlank()) {
+            getString(R.string.screen_driver_license)
+        } else {
+            contactInfoModel.title
+        }
+    }
+
+    private fun setupActions() = with(binding) {
+        toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+        toolbar.setupShare(
+            context = requireContext(),
+            raw = contactInfoModel.raw,
+            subject = contactInfoModel.title ?: contactInfoModel.display,
+        )
     }
 
     override fun onDestroyView() {
