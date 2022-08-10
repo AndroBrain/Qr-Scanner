dcfb7a981d514c34dc4016d88455271579337b3d
Michal Ankiersztajn <aandrobrain@gmail.com>
2022-08-10 14:31:23 +0200

    Ui and Ux improvements

--- app/src/main/java/com/androbrain/qr/scanner/feature/navigation/NavigationFragment.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/navigation/NavigationFragment.kt
@@ -1,6 +1,5 @@
 package com.androbrain.qr.scanner.feature.navigation
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.androbrain.qr.scanner.R
 import com.androbrain.qr.scanner.databinding.FragmentNavigationBinding
-import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.chrisbanes.insetter.applyInsetter
 
@@ -25,31 +23,23 @@ class NavigationFragment : Fragment() {
     private val navController
         get() = navFragment.navController
 
-    private val onItemSelectedListener: NavigationBarView.OnItemSelectedListener =
-        NavigationBarView.OnItemSelectedListener { item ->
-            NavigationUI.onNavDestinationSelected(item, navController)
-        }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNavigationBinding.inflate(inflater, container, false)
-        binding.root.applyInsetter {
-            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                type(navigationBars = true) {
-                    margin()
-                }
-            }
-        }
         setupBottomNav()
         return binding.root
     }
 
     private fun setupBottomNav() = with(binding) {
         NavigationUI.setupWithNavController(navBottomBar, navController)
-        navBottomBar.setOnItemSelectedListener(onItemSelectedListener)
+        navBottomBar.applyInsetter {
+            type(navigationBars = true) {
+                padding()
+            }
+        }
     }
 
     override fun onDestroyView() {
