1904d3b66568062b9b8772e3bdb4a7e3bf475d48
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-08-12 21:54:42 +0200

    Show empty screen when no barcodes are scanned

--- app/src/main/java/com/androbrain/qr/scanner/feature/history/HistoryFragment.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/history/HistoryFragment.kt
@@ -1,19 +1,22 @@
 package com.androbrain.qr.scanner.feature.history
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.androbrain.qr.scanner.NavGraphDirections
 import com.androbrain.qr.scanner.R
 import com.androbrain.qr.scanner.databinding.FragmentHistoryBinding
 import com.androbrain.qr.scanner.feature.history.controller.HistoryController
+import com.androbrain.qr.scanner.util.navigation.safeNavigate
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -34,6 +37,7 @@ class HistoryFragment : Fragment() {
     ): View {
         _binding = FragmentHistoryBinding.inflate(inflater)
         setupViews()
+        setupActions()
         setupObservers()
         return binding.root
     }
@@ -48,12 +52,18 @@ class HistoryFragment : Fragment() {
         recycler.setController(controller)
     }
 
+    private fun setupActions() = with(binding) {
+        layoutHistoryEmpty.buttonScan.setOnClickListener {
+            findNavController().safeNavigate(NavGraphDirections.actionGlobalToScanFragment())
+        }
+    }
+
     private fun setupObservers() = with(binding) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.state.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
                 .onEach { state ->
-                    Log.d("HistoryState", state.toString())
                     controller.barcodes = state.barcodes
+                    layoutHistoryEmpty.root.isVisible = state.barcodes.isEmpty()
                 }.launchIn(this)
         }
     }
--- app/src/main/java/com/androbrain/qr/scanner/feature/history/controller/HistoryController.kt
+++ app/src/main/java/com/androbrain/qr/scanner/feature/history/controller/HistoryController.kt
@@ -1,6 +1,5 @@
 package com.androbrain.qr.scanner.feature.history.controller
 
-import android.util.Log
 import com.airbnb.epoxy.EpoxyController
 import com.androbrain.qr.scanner.feature.history.HistoryBarcode
 import com.androbrain.qr.scanner.feature.history.model.BarcodeCard
@@ -15,7 +14,6 @@ class HistoryController : EpoxyController() {
 
     override fun buildModels() {
         barcodes?.forEachIndexed { index, historyBarcode ->
-            Log.d("HistoryBarcode", historyBarcode.toString())
             BarcodeCard(historyBarcode)
                 .id(index)
                 .addTo(this)
